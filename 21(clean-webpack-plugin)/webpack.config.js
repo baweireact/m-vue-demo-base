6a371a9c9c8df8d5c1339c0f94aceefa5660fa5d
xutongbao <1183391880@qq.com>
2019-09-01 12:31:08 +0800

    webpack

---
+++ 21(clean-webpack-plugin)/webpack.config.js
@@ -0,0 +1,89 @@
+const htmlWebpackPlugin = require("html-webpack-plugin")
+const VueLoaderPlugin = require("vue-loader/lib/plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/main.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'main-[hash].js'
+  },
+  devServer: {
+    contentBase: __dirname + '/dist',
+    port: 8080,
+    inline: true,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+       {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          }, {
+            loader: MiniCssExtractPlugin.loader
+          }, {
+            loader: 'css-loader'
+          }
+        ]
+      }, 
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader'
+          }, {
+            loader: 'sass-loader'
+          }
+        ]
+      }, 
+      {
+        test: /.(png|jpg|gif)$/,
+        use: [
+          {
+            loader:'url-loader',
+            options: {
+              limit: 50000,
+              name: '[path][name]-[hash].[ext]'
+            }
+          }
+        ]
+      }, 
+      {
+        test: /\.vue$/,
+        use: [
+          {
+            loader: 'vue-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: __dirname + '/src/index.html'
+    }),
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    new CleanWebpackPlugin()
+  ]
+}
